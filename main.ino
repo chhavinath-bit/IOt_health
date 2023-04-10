855dab14e75a7a98423c11008aabc4cdadaa1e2f
shrihaxori <ashrihariraju03@gmail.com>
2023-04-10 13:10:54 +0530

    energy code + simplification

--- main.ino
+++ main.ino
@@ -14,15 +14,16 @@ float sensorVoltage;
 float sensorValue;
 float sensorMV;
 int indexUV=0;
+float total, clEnergy, energy = 0;
+
 SimpleTimer timer;
+
 void setup() 
 {
   Serial.begin(9600);
   pinMode(A0, INPUT);
     delay(10);
   connectWifi();
-  
-  
 }
  
  
@@ -37,46 +38,20 @@ void loop()
   sensorValue= sensorValue/5;
   sensorVoltage = (sensorValue/1023)*5;
   sensorMV = sensorVoltage*1000;
-  Serial.print("sensor reading = ");
-  Serial.println(sensorValue);
-  //Serial.println("");
-  Serial.print("sensor voltage = ");
-  Serial.print(sensorVoltage);
-  Serial.println(" V");
-  Serial.print("sensor voltage in mv =");
-  Serial.print(sensorMV);
-  Serial.println(" mV");
- ;
-  if(sensorMV<50){
-    indexUV = 0;
-  } else if(sensorMV<227){
-    indexUV = 1;
-  } else if(sensorMV<318){
-    indexUV = 2;
-  } else if(sensorMV<408){
-    indexUV = 3;
-  } else if(sensorMV<503){
-    indexUV = 4;
-  } else if(sensorMV<606){
-    indexUV = 5;
-  } else if(sensorMV<696){
-    indexUV = 6;
-  } else if(sensorMV<795){
-    indexUV = 7;
-  } else if(sensorMV<881){
-    indexUV = 8;
-  } else if(sensorMV<976){
-    indexUV = 9;
-  } else if(sensorMV<1079){
-    indexUV = 10;
-  } else{
-    indexUV = 11;
-  }
-  Serial.print("index = ");
-  Serial.println(indexUV);
+  Serial.print("sensor reading = ");  Serial.println(sensorValue);
+  Serial.print("sensor voltage = ");  Serial.print(sensorVoltage);  Serial.println(" V");
+  Serial.print("sensor voltage in mv ="); Serial.print(sensorMV); Serial.println(" mV");
+
+  indexUV = uvIndex(sensorMV);
+  Serial.print("index = "); Serial.println(indexUV);
+
+  clEnergy = energyCalculate(indexUV);
+  Serial.print("Total Energy on unit area until now "); Serial.print(clEnergy); Serial.println(" W-s/m^2")
+
   delay(1000);
   sendDataTS();
 }
+
 void connectWifi()
 {
   Serial.print("Connecting to "+ *MY_SSID);
@@ -90,6 +65,7 @@ void connectWifi()
   Serial.println("WiFi Connected");
   Serial.println("");  
 }
+
 void sendDataTS(void)
 {
      
@@ -127,4 +103,42 @@ void sendDataTS(void)
   client.stop();
   Serial.println("Waiting...");
  
+}
+
+int uvIndex(int sensor){
+  int index;
+  if(sensor<50){
+    index = 0;
+  } else if(sensor<227){
+    index = 1;
+  } else if(sensor<318){
+    index = 2;
+  } else if(sensor<408){
+    index = 3;
+  } else if(sensor<503){
+    index = 4;
+  } else if(sensor<606){
+    index = 5;
+  } else if(sensor<696){
+    index = 6;
+  } else if(sensor<795){
+    index = 7;
+  } else if(sensor<881){
+    index = 8;
+  } else if(sensor<976){
+    index = 9;
+  } else if(sensor<1079){
+    index = 10;
+  } else{
+    index = 11;
+  }
+  return index;
+}
+
+float energyCalculate(int index){
+  int time_rect = 1;  // in seconds
+  float uvIntensity = index/40;
+  float energy  = (uvIntensity*time_rect)
+  total += energy;
+  return total;
 }
